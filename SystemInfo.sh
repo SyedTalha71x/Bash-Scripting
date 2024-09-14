7e8597a1fe2df950632f31066aafd19ecabfc183
SyedTalha71x <syedtalha71x@gmail.com>
2024-09-14 20:52:31 +0000

    first commit

---
+++ SystemInfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This scripts print System Info
+
+echo "Welcome to Bash Scripting DevOps"
+
+echo "---------------------------------"
+
+echo "Memory Utilization Setup"
+free -m
---
+++ WebsiteTemplate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Sample Script to setup a website 
+
+echo "Installing all dependencies"
+echo "---------------------------"
+sudo apt --fix-broken install -y
+sudo apt install wget unzip apache2 -y > /dev/null
+
+
+echo "Starting and Enabling Apache Service"
+echo "------------------------------------"
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+
+echo "Starting Artifact Deployment"
+echo "----------------------------"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+                   
+wget https://www.tooplate.com/zip-templates/2134_gotto_job.zip > /dev/null
+unzip 2134_gotto_job.zip > /dev/null
+sudo cp -rf 2134_gotto_job/* /var/www/html/
+echo
+
+echo "Restarting Apache Service"
+echo "------------------------"
+sudo systemctl restart apache2
+echo
+
+echo "Removing Temporary Files"
+echo "------------------------"
+rm -rf /tmp/webfiles
+echo
+
+# Checking Apache service status and listing HTML files
+sudo systemctl status apache2
+ls /var/www/html/
+
