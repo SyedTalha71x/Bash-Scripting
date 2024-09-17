16589eedddb15df1c372b304dfd563ca142e17d0
SyedTalha71x <syedtalha71x@gmail.com>
2024-09-17 18:56:21 +0000

    updates

--- Loops.sh
+++ Loops.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 
-for VAR in java ansible maven
+#for VAR in java ansible maven
+#do
+#   echo "Looping"
+#   echo "-------"
+#   sleep 2
+#   echo "Value of var is $VAR"
+#   echo ""
+#done
+
+
+COUNTER=0
+
+while [ $COUNTER -lt 5 ]
 do
-   echo "Looping"
-   echo "-------"
-   sleep 2
-   echo "Value of var is $VAR"
-   echo ""
+   echo "Entering in a while loop"
+   echo "Value for Counter is $COUNTER"
+   COUNTER=$(( $COUNTER + 1 ))
+   sleep 1
 done
+
+echo "Out of the loop"
+
